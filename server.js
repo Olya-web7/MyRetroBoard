ceddfb51b56fe997bbc63bb06da3730be49ca3bd
Olya <shyshova24@gmail.com>
2021-11-30 20:26:25 +0200

    fix

--- server.js
+++ server.js
@@ -1,50 +1,10 @@
-// const app = require('./backend/app');
-// const debug = require('debug')("node-angular");
-// const http = require('http');
+const express = require('express');
+const path = require('path');
+const app = express();
 
-// const normalizePort = val => {
-//   let port = parseInt(val, 10);
+app.use(express.static(__dirname + '/dist/MB'));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/MB'));
+});
 
-//   if (isNaN(port)) {
-//     return val;
-//   }
-
-//   if (port >= 0) {
-//     return  port;
-//   }
-
-//   return port;
-// }
-
-// const onError = error => {
-//   if (error.syscall !== "listen") {
-//     throw error;
-//   }
-//   const bind = typeof addr === "string" ? "pipe" + addr : "port " + port;
-//   switch (error.code) {
-//     case "EACCES":
-//       console.error(bind + " requires elevated privileges");
-//       process.exit(1);
-//       break;
-//     case "EADDRINUSE":
-//       console.error(bind + " is alredy in use");
-//       process.exit(1);
-//       break;
-//     default:
-//       throw error;
-//   }
-// };
-
-//   const onListening = () => {
-//     const addr = server.address();
-//     const bind = typeof addr === "string" ? "pipe" + addr : "port " + port;
-//     debug("Listening on " + bind);
-//   };
-
-// const port = normalizePort(process.env.PORT || "3000");
-// app.set('port', port);
-
-// const server = http.createServer(app);
-// server.on("error", onError);
-// server.on("listening", onListening);
-// server.listen(port);
+app.listen(process.env.PORT || 8080);
